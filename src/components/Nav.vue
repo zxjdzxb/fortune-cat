bef8d7dea75ad9f254ce6d31927f9d6d850f14fe
周兴杰 <1660154581@qq.com>
2022-07-16 09:35:21 +0800

    Money页面布局

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,11 @@
 <template>
-  <div>
-    <hr/>
-    <div>
+    <div class="nav">
       <router-link to="/money">记账</router-link>
       |
       <router-link to="/labels">标签</router-link>
       |
       <router-link to="/statistics">统计</router-link>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -18,5 +15,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav{
+  border:1px solid red;
+}
 
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>money
+  <div class="nav-wrapper">
+
+    <div class="content"> money</div>
   <Nav/>
   </div>
 </template>
@@ -12,5 +14,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper{
+  border:1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content{
+  border: 1px solid blue;
+  overflow: auto;
+  flex-grow: 1;
+}
 
 </style>
