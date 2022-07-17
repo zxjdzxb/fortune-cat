c9f6a6023f195adf027235e21a319ee4974dc49e
周兴杰 <1660154581@qq.com>
2022-07-17 10:41:24 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -40,7 +41,7 @@ nav {
     }
   }
   > .item.selected{
-    color: #cb7635;
+    color: $color-highlight;
   }
 }
 
