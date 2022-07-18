87f5f9feed786d0b9372d09a434ae0c3c0069db9
周兴杰 <1660154581@qq.com>
2022-07-18 18:19:11 +0800

    收集四个组件的数据到 record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="numberPad">
-    <div class="output">{{output }} </div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
@@ -13,7 +13,7 @@
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button  class="ok">OK</button>
+      <button @click="ok" class="ok">OK</button>
       <button @click="inputContent" class="zero">0</button>
       <button @click="inputContent">.</button>
     </div>
@@ -26,8 +26,10 @@ import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class NumberPad extends Vue {
-  output = "0";
-  
+  @Prop() readonly value!: number;
+  output = this.value.toString();
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   inputContent(event: MouseEvent) {
     const button = (event.target as HTMLButtonElement);
     const input = button.textContent as string;
@@ -44,6 +46,8 @@ export default class NumberPad extends Vue {
     if (this.output.indexOf('.') >= 0 && input === '.') {return;}
     this.output += input;
   }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   remove() {
     if (this.output.length === 1) {
       this.output = '0';
@@ -51,10 +55,17 @@ export default class NumberPad extends Vue {
       this.output = this.output.slice(0, -1);
     }
   }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   clear() {
     this.output = '0';
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  ok() {
+    this.$emit('update:value', this.output);
+
+  }
 }
 
 </script>
