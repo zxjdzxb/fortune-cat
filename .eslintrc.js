cde926372a56f5d7f24aa99813fc15eddbefe52c
周兴杰 <1660154581@qq.com>
2022-07-19 08:49:21 +0800

    封装出一个 model

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "globals": { "RecordItem": true },
   root: true,
   env: {
     node: true
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
+    {{ record }}
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
@@ -14,38 +14,38 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component,Watch} from 'vue-property-decorator';
-const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-type Record = {
-  tags: string[]
-  notes: string
-  type: string
-  amount: number // 数据类型 object | string
-  createdAt?: Date  // 类 / 构造函数
-}
+import {Component, Watch} from 'vue-property-decorator';
+import model from '@/model';
+
+const recordList = model.fetch();
+
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行', '彩票'];
-  recordList: Record[] = recordList;
-  record: Record = {
+  recordList: RecordItem[] = recordList;
+  record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
+
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
+
   onUpdateNotes(value: string) {
     this.record.notes = value;
   }
+
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
+
   @Watch('recordList')
   onRecordListChange() {
-    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+    model.save(this.recordList);
   }
 }
 </script>
