a086c1844d3ab9ff1b52c7525659f2ef724d4316
周兴杰 <1660154581@qq.com>
2022-07-19 18:45:24 +0800

    封装 tagList 的增删改查和封装 record 的操作

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -20,5 +20,11 @@ type TagListModel = {
 }
 
 interface Window {
-  tagList: Tag[]
+  tagList: Tag[];
+  createTag: (name: string) => void;
+  findTag: (id: string) => Tag | undefined;
+  removeTag: (id: string) => boolean;
+  updateTag: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  recordList: RecordItem[];
+  createRecord: (record: RecordItem) => void;
 }
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,18 +19,16 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component, Watch} from 'vue-property-decorator';
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import {Component} from 'vue-property-decorator';
 
-const recordList = recordListModel.fetch();
+
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad}
 })
 export default class Money extends Vue {
   tags = window.tagList;
-  recordList: RecordItem[] = recordList;
+  recordList = window.recordList;
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
@@ -41,11 +39,7 @@ export default class Money extends Vue {
     this.record.notes = value;
   }
   saveRecord() {
-    recordListModel.create(this.record);
-  }
-  @Watch('recordList')
-  onRecordListChange() {
-    recordListModel.save();
+    window.createRecord(this.record);
   }
 }
 </script>
