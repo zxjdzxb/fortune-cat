af1ea7dfa39efdf387017740135558e66a433978
周兴杰 <1660154581@qq.com>
2022-07-19 16:00:50 +0800

    id生成器

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -47,7 +47,11 @@ export default class EditLabel extends Vue {
   }
   remove() {
     if (this.tag) {
-      tagListModel.remove(this.tag.id);
+      if (tagListModel.remove(this.tag.id)) {
+        this.$router.back();
+      } else {
+        window.alert('删除失败');
+      }
     }
   }
   goBack() {
